615198f544e560aecf1434d37e293a748a0aa934
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-17 14:01:29 +0545

    Basics

---
+++ Basics/demo.java
@@ -0,0 +1,21 @@
+package Basics;
+public class demo{
+    public static void main(String args[]) {
+    System.out.println(args[0]); // Bikram
+    System.out.println(args[1]); // 20
+}
+
+
+}
+/* While running class file we pass two strings which will be stored in args[] array
+and print both values in array
+
+eg:
+javac demo.java
+java demo beeks code 
+output:
+beeks // from args[0]
+code // from args[1]
+
+
+*/
