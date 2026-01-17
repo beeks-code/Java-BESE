908f9ada002d636fa3b683bee23217dccce9178e
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-17 14:13:41 +0545

    add change

--- basic.java
+++ basic.java
@@ -3,8 +3,8 @@ public class basic {
         float price=(float) 10.2; // casting to float type
         float val=10.2f; // alternative of above statement
 
-        System.out.println("First value is "+price);
-        System.out.println("Second value is "+val);
+        System.out.println("First price value is "+price);
+        System.out.println("Second price value is "+val);
 
         String text=new String("Price is");
         String concat= "Hi" + text + val;
