1b3b8bae8686d2ffb400c5f456c4f0b2231eb934
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-17 14:14:48 +0545

    add change

---
+++ Basics/basic.java
@@ -0,0 +1,15 @@
+package Basics;
+public class basic {
+    public static void main(String[] args) {
+        float price=(float) 10.2; // casting to float type
+        float val=10.2f; // alternative of above statement
+
+        System.out.println("First price value is "+price);
+        System.out.println("Second price value is "+val);
+
+        String text=new String("Price is");
+        String concat= "Hi" + text + val;
+        System.out.println(concat);
+
+    }
+}
