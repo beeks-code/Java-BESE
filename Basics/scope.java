1341e37dfbd590ac5a46222f5ec471229532e484
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-18 10:31:57 +0545

    add scope

---
+++ Basics/scope.java
@@ -0,0 +1,14 @@
+package Basics;
+public class scope {
+    public static void main(String args[]){
+        int x=10;
+        if(true)
+        {
+            int localvar=100; // limited within this block 
+            x=20; // x is already declared outside block so, it is possible to get it inside the block
+            System.out.println("Local value of x is "+x);
+        }
+        System.out.println("Outside block value of x is"+x); //20
+        // System.out.println(localvar);  
+    }
+}
