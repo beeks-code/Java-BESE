c03f1cf7de30f2bf12ecc96b9d3a7411cdc3adb8
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-18 22:01:50 +0545

    add ops

--- OOP/basicClass.java
+++ OOP/basicClass.java
@@ -1,5 +1,19 @@
 package OOP;
-
 public class basicClass {
-    
+    public static void main(String[] args) {
+        student std1= new student();
+        std1.name="Beeks";
+        std1.age=21;
+        std1.height=1.61f; // in meter
+        int height_cm=(int)(std1.height *100);
+        System.out.printf("%s who studies in %s is %dcm tall and %d yrs old",std1.name,std1.school,height_cm,std1.age);
+
+    }
+}
+
+class student{
+    int age;
+    String name, school="BMSS";
+    float height;
+
 }
