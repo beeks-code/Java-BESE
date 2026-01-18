fe3efcbbbd8d126cbd7c3d16f39dc9de925ddc3d
Bikram Sharma <bekrmsharma@gmail.com>
2026-01-18 10:43:06 +0545

    adjust

--- Basics/scope.java
+++ Basics/scope.java
@@ -5,6 +5,7 @@ public class scope {
         int x=10;
         if(true)
         {
+            // int x=20; // redeclaration of variable  throws error
             int localVar=100; // limited within this block 
             x=20; // x is already declared outside block so, it is possible to get it inside the block
             System.out.println("Local value of x is "+x);
@@ -12,4 +13,5 @@ public class scope {
         System.out.println("Outside block value of x is"+x); //20
         // System.out.println(localvar);  
     }
+
 }
